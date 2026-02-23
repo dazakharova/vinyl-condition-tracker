75860a5e63efb2e245091710810842aecaef6c96
Daria Zakharova <daria.zakharova1404@gmail.com>
2026-02-23 22:59:27 +0200

    Move routes into separate file

--- cmd/server/routes.go
+++ cmd/server/routes.go
@@ -5,7 +5,7 @@ import "net/http"
 func (app *application) routes() *http.ServeMux {
 	mux := http.NewServeMux()
 
-	mux.HandleFunc("GET /{$}", home)
+	mux.HandleFunc("GET /{$}", app.home)
 	mux.HandleFunc("GET /record/view/{id}", recordView)
 	mux.HandleFunc("GET /record/create", recordCreate)
 
