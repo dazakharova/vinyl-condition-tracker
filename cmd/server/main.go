52389b7850a7c5a5b6d67d6728858ead2f610620
Daria Zakharova <daria.zakharova1404@gmail.com>
2026-02-23 16:11:16 +0200

    Read port from environment variable

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -3,16 +3,22 @@ package main
 import (
 	"log"
 	"net/http"
+	"os"
 )
 
 func main() {
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "4001"
+	}
+
 	mux := http.NewServeMux()
 	mux.HandleFunc("GET /{$}", home)
 	mux.HandleFunc("GET /record/view/{id}", recordView)
 	mux.HandleFunc("GET /record/create", recordCreate)
 
-	log.Println("Starting server on port :4000")
+	log.Printf("Starting server on port :%s", port)
 
-	err := http.ListenAndServe(":4000", mux)
+	err := http.ListenAndServe(":"+port, mux)
 	log.Fatal(err)
 }
