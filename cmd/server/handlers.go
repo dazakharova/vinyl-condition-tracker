73c70bf72d8c0f5ccaf99c02057a2cab6d2bb22d
Daria Zakharova <daria.zakharova1404@gmail.com>
2026-02-23 15:22:25 +0200

    Create and use HTML template for home page

--- cmd/server/handlers.go
+++ cmd/server/handlers.go
@@ -2,12 +2,30 @@ package main
 
 import (
 	"fmt"
+	"html/template"
+	"log"
 	"net/http"
 	"strconv"
 )
 
 func home(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("Hello from Vinyl Record Condition Tracker"))
+	files := []string{
+		"./ui/html/base.tmpl",
+		"./ui/html/pages/home.tmpl",
+	}
+
+	ts, err := template.ParseFiles(files...)
+	if err != nil {
+		log.Println(err.Error())
+		http.Error(w, "internal Server Error", http.StatusInternalServerError)
+		return
+	}
+
+	err = ts.ExecuteTemplate(w, "base", nil)
+	if err != nil {
+		log.Println(err.Error())
+		http.Error(w, "internal Server Error", http.StatusInternalServerError)
+	}
 }
 
 func recordView(w http.ResponseWriter, r *http.Request) {
