fece67846278d95e0d9cc70fa54242da134ac458
Daria Zakharova <daria.zakharova1404@gmail.com>
2026-02-23 15:22:00 +0200

    Move route handlers from main.go to separate directory

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,30 +1,9 @@
 package main
 
 import (
-	"fmt"
 	"log"
 	"net/http"
-	"strconv"
 )
-
-func home(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("Hello from Vinyl Record Condition Tracker"))
-}
-
-func recordView(w http.ResponseWriter, r *http.Request) {
-	id, err := strconv.Atoi(r.PathValue("id"))
-	if err != nil || id < 1 {
-		http.NotFound(w, r)
-		return
-	}
-
-	msg := fmt.Sprintf("Display a specific record with ID %d", id)
-	w.Write([]byte(msg))
-}
-
-func recordCreate(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("Display a form for creating a new record"))
-}
 
 func main() {
 	mux := http.NewServeMux()
