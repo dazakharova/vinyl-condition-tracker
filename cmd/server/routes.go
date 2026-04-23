34a1a7693b4c8296fe968003c611531486aac48f
Daria Zakharova <daria.zakharova1404@gmail.com>
2026-04-23 15:34:42 +0300

    Style record pages and serve static CSS assets

--- cmd/server/routes.go
+++ cmd/server/routes.go
@@ -5,6 +5,9 @@ import "net/http"
 func (app *application) routes() *http.ServeMux {
 	mux := http.NewServeMux()
 
+	fileServer := http.FileServer(http.Dir("./ui/static"))
+	mux.Handle("GET /static/", http.StripPrefix("/static/", fileServer))
+
 	mux.HandleFunc("GET /{$}", app.home)
 	mux.HandleFunc("GET /record/view/{id}", app.recordView)
 	mux.HandleFunc("GET /record/create", app.recordCreate)
